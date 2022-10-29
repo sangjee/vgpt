dbfdbc98b9f6898824afe99214922a548f2d1632
unknown <hamusj627@gmail.com>
2022-10-29 16:16:08 +0900

    .

--- evaluation/tokenizer.py
+++ evaluation/tokenizer.py
@@ -45,8 +45,7 @@ class PTBTokenizer(object):
         # tokenize sentence
         cmd.append(os.path.basename(tmp_file.name))
 
-        p_tokenizer = subprocess.Popen(cmd, cwd=path_to_jar_dirname, \
-                stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
+        p_tokenizer = subprocess.Popen(cmd, cwd=path_to_jar_dirname,stdout=subprocess.PIPE)
         token_lines = p_tokenizer.communicate(input=sentences.rstrip())[0]
 
         token_lines = token_lines.decode()
