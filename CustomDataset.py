71fe33ef3446839678b5843e4e2932df8d639a1e
unknown <hamusj627@gmail.com>
2022-11-11 11:02:46 +0900

    .

--- CustomDataset.py
+++ CustomDataset.py
@@ -7,7 +7,7 @@ import cv2
 import albumentations as A
 
 class CustomDataset(Dataset):
-  def __init__(self, image_filenames, captions, image_input1, image_input2, image_input3, tokenizer):
+  def __init__(self, image_filenames, captions, image_input1, image_input2, image_input3, tokenizer, transforms):
     self.image_filenames = image_filenames
     self.image_input1 = image_input1
     self.image_input2 = image_input2
@@ -17,7 +17,7 @@ class CustomDataset(Dataset):
     #     list(captions)
     # )
     self.tokenizer = tokenizer
-    # self.transforms = transforms
+    self.transforms = transforms
     
   def __len__(self):
     return len(self.captions)
@@ -37,6 +37,7 @@ class CustomDataset(Dataset):
         image_3d[:,:,1] = image2
         image_3d[:,:,2] = image3
     hf.close()
+    image_3d = self.transforms(image=image_3d)['image']
     item['image'] = torch.tensor(image_3d).permute(2, 0, 1).float()
     # item['image'] = item['image'].reshape(-1,224)
     # item['text'] = self.captions[index]
@@ -48,7 +49,7 @@ class CustomDataset(Dataset):
 def _get_image(image):
     image = np.array(image)
     image = image.astype(np.float32)
-    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
-    image = cv2.resize(image, dsize=(256, 256))
+    # image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+    # image = cv2.resize(image, dsize=(256, 256))
 
     return image
